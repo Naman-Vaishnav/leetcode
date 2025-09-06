a50dfe0fc1781ff8750289919ca2ccbd72ce434b
Naman vaishnav <Vaishnavnaman1199@gmail.com>
2025-09-07 00:27:28 +0530

    Time: 12 ms (56.84%) | Memory: 42.7 MB (18.35%) - LeetSync

--- 1203-print-in-order/print-in-order.java
+++ 1203-print-in-order/print-in-order.java
@@ -1,52 +1,22 @@
 class Foo {
-    int cur=1;
-    Object lock = new Object();
-    public Foo() {
-        
-    }
+    private volatile int cur = 1;
+
+    public Foo() {}
 
     public void first(Runnable printFirst) throws InterruptedException {
-        
-         synchronized (lock) {
-             while(cur!=1){
-              lock.wait();   
-            }
-             printFirst.run();
-            cur++;
-            lock.notifyAll(); 
-
-         }
-        
+        while (cur != 1) { } // spin wait
+        printFirst.run();
+        cur = 2;
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        
-        synchronized (lock) {
-             while(cur!=2){
-              lock.wait();   
-            }
-             printSecond.run();
-            cur++;
-            lock.notifyAll(); 
-
-         }
-        
-          
-        
+        while (cur != 2) { } // spin wait
+        printSecond.run();
+        cur = 3;
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        
-        synchronized (lock) {
-             while(cur!=3){
-              lock.wait();   
-            }
-             printThird.run();
-            cur++;
-            lock.notifyAll(); 
-
-         }
-        
-       
+        while (cur != 3) { } // spin wait
+        printThird.run();
     }
 }
