435b4a4e4bb3ee41451587b7de83129531e87de4
Naman vaishnav <Vaishnavnaman1199@gmail.com>
2025-10-20 13:48:24 +0530

    Time: 1 ms (74.53%) | Memory: 42.4 MB (96.36%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int ans=0;
+        for(String x:operations){
+            if(x.charAt(0)=='+'||x.charAt(2)=='+')ans++;
+            if(x.charAt(0)=='-'||x.charAt(2)=='-')ans--;
+        }
+        return ans;
+    }
+}
