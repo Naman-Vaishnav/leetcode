f0db3d396e1f541f59a19e315d31256c22a0e6ed
Naman vaishnav <Vaishnavnaman1199@gmail.com>
2025-12-20 16:22:26 +0530

    Time: 4 ms (77.85%) | Memory: 47.2 MB (7.48%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,11 +10,32 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode ans=null;
-        for(ListNode cur:lists){
-            ans=merge(ans,cur);
+        //N->size in each list
+        //N*K*K (N+2*N+3*N+...+K*N)
+        // ListNode ans=null;
+        // for(ListNode cur:lists){
+        //     ans=merge(ans,cur);
+        // }
+        // return ans;
+
+        //N LogK
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->Integer.compare(a.val,b.val));
+
+        for(ListNode cur:lists)
+            if(cur!=null)pq.add(cur);
+        ListNode head=null,tail=null;
+        while(pq.size()>0){
+
+            ListNode min=pq.poll();
+            if(head==null)head=min;
+            if(tail!=null)tail.next=min;
+            tail=min;
+
+            if(min.next!=null)pq.add(min.next);
+
         }
-        return ans;
+        return head;
+
     }
 
     ListNode merge(ListNode l1,ListNode l2){
