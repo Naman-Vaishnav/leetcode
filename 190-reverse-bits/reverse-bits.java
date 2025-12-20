9c58cf148da163b9b0a9318369454f9adc9b3ff8
Naman vaishnav <Vaishnavnaman1199@gmail.com>
2025-12-20 23:21:27 +0530

    Time: 1 ms (50.11%) | Memory: 42.7 MB (8.98%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -5,7 +5,7 @@ public class Solution {
         for(int i=0;i<32;i++){
             int bit=n&1;
             ans=(ans<<1)|bit;
-            n>>=1;
+            n>>>=1;
         } 
         return ans;
     }
