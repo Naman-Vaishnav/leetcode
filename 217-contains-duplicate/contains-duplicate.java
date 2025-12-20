e065bc206a81c288532bd229acb0b1f9f7cf8129
Naman vaishnav <Vaishnavnaman1199@gmail.com>
2025-12-20 14:31:30 +0530

    Time: 14 ms (81.63%) | Memory: 93.3 MB (46.48%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hs=new HashSet<>();
+        for(int x:nums){
+            if(hs.contains(x))return true;
+            hs.add(x);
+        }
+        return false;
+    }
+}
+
+/** 
+sort time N lonN
+HashSet space N
+*/
