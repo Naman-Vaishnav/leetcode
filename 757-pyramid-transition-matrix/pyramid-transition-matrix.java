f62931f55c1c14251e279f653cdd394aa9651212
Naman vaishnav <Vaishnavnaman1199@gmail.com>
2025-12-29 20:11:01 +0530

    Time: 114 ms (65.61%) | Memory: 49.5 MB (17.83%) - LeetSync

--- 757-pyramid-transition-matrix/pyramid-transition-matrix.java
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.java
@@ -1,42 +1,60 @@
 class Solution {
-    Map<String, List<Character>> childs = new HashMap<>();
-    Set<String> bad = new HashSet<>();
-
+    Map<String,List<String>> childs;
+    Map<String,Boolean> dp;
     public boolean pyramidTransition(String bottom, List<String> allowed) {
-        for (String cur : allowed) {
-            String key = cur.substring(0, 2);
-            childs.computeIfAbsent(key, k -> new ArrayList<>())
-                  .add(cur.charAt(2));
+        childs=new HashMap<>();
+        dp=new HashMap<>();
+        for(String cur:allowed){
+            String key=cur.substring(0,2);
+            String value=cur.substring(2);
+            List<String> list=childs.get(key);
+            if(list==null)list=new ArrayList<>();
+            list.add(value);
+            childs.put(key,list);
         }
-
-        for (int i = 0; i < bottom.length() - 1; i++) {
-            if (!childs.containsKey(bottom.substring(i, i + 2)))
-                return false;
-        }
-
-        return isPossible(0, bottom, new StringBuilder());
+        StringBuilder next=new StringBuilder();
+        return isPossible(0,bottom,next);
     }
 
-    boolean isPossible(int i, String cur, StringBuilder next) {
-        if (i == cur.length() - 1) {
-            if (next.length() == 1) return true;
+    boolean isPossible(int i,String cur,StringBuilder next){
 
-            String row = next.toString();
-            if (bad.contains(row)) return false;
+        
 
-            boolean ok = isPossible(0, row, new StringBuilder());
-            if (!ok) bad.add(row);
-            return ok;
+        if(i==cur.length()-1){
+            if(next.length()==1)return true;
+
+            if(dp.get(next.toString())!=null)return dp.get(next.toString());
+
+            boolean possible=isPossible(0,next.toString(),new StringBuilder());
+            dp.put(next.toString(),possible);
+
+            if(possible)return true;
+            return false;
         }
-
-        String key = "" + cur.charAt(i) + cur.charAt(i + 1);
-        if (!childs.containsKey(key)) return false;
-
-        for (char ch : childs.get(key)) {
+        //System.out.println(cur);
+        String key=cur.charAt(i)+""+cur.charAt(i+1);
+        if(!childs.containsKey(key))return false;
+        for(String ch:childs.get(key)){
             next.append(ch);
-            if (isPossible(i + 1, cur, next)) return true;
-            next.deleteCharAt(next.length() - 1);
+            if(isPossible(i+1,cur,next))return true;
+            next.deleteCharAt(next.length()-1);
         }
+
+
+
         return false;
+
     }
 }
+
+/**
+solve(i,cur,next)
+    if(i==cur.length()){
+        if(next.length()==1)return true;
+        if(isPossible(0,next,"")) return true;
+        else return false;
+    }
+    key=i ,i+1
+    for(childs)
+        if(solve(i+2,cur,next+[ch]))
+ */
