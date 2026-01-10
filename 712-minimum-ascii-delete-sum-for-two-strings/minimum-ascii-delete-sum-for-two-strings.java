b38722f0513e869444095808d0f598b6ad073841
Naman vaishnav <Vaishnavnaman1199@gmail.com>
2026-01-10 23:01:57 +0530

    Time: 35 ms (6.56%) | Memory: 48.2 MB (8.35%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,8 +1,30 @@
 class Solution {
     Integer[][] dp;
     public int minimumDeleteSum(String s1, String s2) {
-        dp=new Integer[s1.length()][s2.length()];
-        return findMin(0,0,s1,s2);
+        int n=s1.length(),m=s2.length();
+        dp=new Integer[n+1][m+1];
+        //return findMin(0,0,s1,s2);
+
+        dp[0][0]=0;
+        for(int j=1;j<=m;j++){
+            dp[0][j]=dp[0][j-1]+s2.charAt(j-1);
+        }
+        for(int i=1;i<=n;i++){
+            dp[i][0]=dp[i-1][0]+s1.charAt(i-1);
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1))dp[i][j]=dp[i-1][j-1];
+                else{
+                    int delI=s1.charAt(i-1)+dp[i-1][j];
+                    int delJ=s2.charAt(j-1)+dp[i][j-1];
+                    int delIJ=s1.charAt(i-1)+s2.charAt(j-1)+dp[i-1][j-1];
+                    dp[i][j]=Math.min(delI,Math.min(delJ,delIJ));
+                }
+            }
+        }
+        return dp[n][m];
+
     }
 
     int findMin(int i,int j,String s1, String s2){
@@ -35,6 +57,9 @@ class Solution {
 /*
 delete"
 "leet"
+(0,0) 0
+(0,j) sum(j)
+(i,0) sum(i)
 
 (i,j)
     if(i<0||j<0){
